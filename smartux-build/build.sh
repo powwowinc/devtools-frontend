b0300c9c70849e4da1dcde8ceb761b0665699f5a
Samir Mahendra <samir@powwowmobile.com>
2020-03-06 21:34:44 -0800

    Updated built script

--- smartux-build/build.sh
+++ smartux-build/build.sh
@@ -1,28 +1,32 @@
+#!/bin/sh
+
 # Run this script to build the inspector. The built "inspector" folder needs to go in smartux-build/inspector,
 # and is checked in.  Tag it with a new # and update that # the PowwowDesigner/webapp/bower.json file.
 
-# Clone Chromium depot_tools used to build.
+echo "* Cleaning out existing inspector build"
+rm -rf inspector
+
+echo "* Cloning Chromium depot_tools used to perform the build"
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
-# Replace the Chromium devtools-frontend repository location with Powwow's.
+echo "* Updating Chromium devtools-frontend repository location with PowWow's"
 sed -i '' -f devtools_to_powwow.sed depot_tools/fetch_configs/devtools-frontend.py
 sed -i '' -f devtools_to_powwow.sed depot_tools/recipes/recipe_modules/gclient/config.py
 
-# Add depot tools to the path.
+echo "* Adding depot tools to the path"
 export PATH=$PWD/depot_tools:$PATH
 
-# Get the devtools-frontend (without history so it downloads faster)
+echo "* Fetching the devtools-frontend (without history so it downloads faster)"
 fetch --no-history devtools-frontend
 
-# Build the devtools-frontend
+echo "* Building the devtools-frontend using Google's tools"
 cd devtools-frontend
 gn gen out/Default
 autoninja -C out/Default
 
-# Clean out existing resources, and copy the updated built resources to the top level (to this folder)
-rm -rf inspector
+echo "* Copying the updated built resources to the top level (to this folder)"
 mv out/Default/resources/inspector ../inspector
 
-# Cleanup
+echo "* Cleaning up everything that was downloaded"
 cd ..
 rm -rf devtools-frontend depot_tools .gclient_entries .gclient .cipd
