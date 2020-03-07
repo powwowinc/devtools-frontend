e1b24615bc80aeacf5efcff657bf568544b72642
Samir Mahendra <samir@powwowmobile.com>
2020-03-06 21:17:06 -0800

    Updated build script

--- smartux-build/build.sh
+++ smartux-build/build.sh
@@ -1,30 +1,27 @@
-# Run this script to build the inspector.
-# The built "inspector" folder needs to go here:
-# (NOTE: inspector should be renamed "front_end")
-# "PowwowDesigner/webapp/app/bower_components/devtools/front_end
+# Run this script to build the inspector. The built "inspector" folder needs to go in smartux-build/inspector,
+# and is checked in.  Tag it with a new # and update that # the PowwowDesigner/webapp/bower.json file.
 
 # Clone Chromium depot_tools used to build.
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-# Replace the Chromium devtools-frontend repot location with Powwow's.
+
+# Replace the Chromium devtools-frontend repository location with Powwow's.
 sed -i '' -f devtools_to_powwow.sed depot_tools/fetch_configs/devtools-frontend.py
 sed -i '' -f devtools_to_powwow.sed depot_tools/recipes/recipe_modules/gclient/config.py
-# Add depot tools to the path.
-export PATH=./depot_tools:$PATH
-# Get the devtools-frontend.
-fetch --no-history devtools-frontend
 
-# OR, to build a different branch other than master:
-# fetch devtools-frontend
-# cd devtools-frontend
-# git checkout smartux-explorer-integration
-# cd ..
+# Add depot tools to the path.
+export PATH=$PWD/depot_tools:$PATH
+
+# Get the devtools-frontend (without history so it downloads faster)
+fetch --no-history devtools-frontend
 
 # Build the devtools-frontend
 cd devtools-frontend
 gn gen out/Default
 autoninja -C out/Default
-# Copy the build resources to the top level
-mv out/Default/resources/inspector ..
+
+# Clean out existing resources, and copy the updated built resources to the top level (to this folder)
+rm -rf inspector
+mv out/Default/resources/inspector ../inspector
 
 # Cleanup
 cd ..
